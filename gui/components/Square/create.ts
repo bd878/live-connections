3a8574e8ab2f7114e6a37a1eba15e7b9ee4b4cd4
teralion <slavad878@gmail.com>
2023-04-08 17:51:51 +0300

    components/Square append in order titlesList, textArea

--- gui/components/Square/create.ts
+++ gui/components/Square/create.ts
@@ -4,15 +4,15 @@ import TitlesList from '../TitlesList';
 import create from '../../methods/create';
 
 function createSquare(this: Square, id: Id = ''): HTMLElement {
-  const textarea = new TextArea();
-  textarea.create(id);
-
   const titlesList = new TitlesList();
   titlesList.create(id);
 
+  const textarea = new TextArea();
+  textarea.create(id);
+
   const root = create.call(this, id);
-  this.addElem(textarea.getUid(), textarea);
   this.addElem(titlesList.getUid(), titlesList);
+  this.addElem(textarea.getUid(), textarea);
 
   return root;
 }
